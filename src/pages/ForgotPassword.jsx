c7d9aa2b007652b7427903820c7a2ab10bd7a04e
rizalarif <rizal.arif2001@gmail.com>
2022-10-30 19:44:24 +0700

    refactor: use formik yup for forgot/reset password

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,29 +1,70 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
 import http from '../helpers/http';
 
 function ForgotPassword() {
   const navigate = useNavigate();
-  const forgotAction = async (e) => {
+
+  const basicAuthSchema = Yup.object().shape({
+    email: Yup.string().email('Email is not valid').required(),
+  });
+
+  const submitAction = async (values) => {
     try {
-      e.preventDefault();
-      const email = e.target.email.value;
-      await http().post('/auth/forgot-password', email);
+      const form = new URLSearchParams(values);
+      await http().post('/auth/forgot-password', form.toString());
       navigate('/reset-password');
     } catch (err) {
-      // eslint-disable-next-line no-alert
       window.alert(err.response.data.message);
     }
   };
+
+  // const forgotAction = async (e) => {
+  //   try {
+  //     e.preventDefault();
+  //     const email = e.target.email.value;
+  //     await http().post('/auth/forgot-password', email);
+  //     navigate('/reset-password');
+  //   } catch (err) {
+  //     // eslint-disable-next-line no-alert
+  //     window.alert(err.response.data.message);
+  //   }
+  // };
+
   return (
-    <form onSubmit={forgotAction}>
-      Input email address, to send the confirmation code
-      <br />
-      <input type="email" name="email" />
-      <br />
-      <button type="submit">Submit</button>
-    </form>
+    <Formik
+      initialValues={{
+        email: '',
+      }}
+      validationSchema={basicAuthSchema}
+      onSubmit={submitAction}
+    >
+      {({ errors, touched }) => (
+        <Form>
+          Email :
+          <Field type="text" name="email" />
+          <br />
+          {errors.email && touched.email ? (
+            <div>{errors.email}</div>
+          ) : null}
+          <br />
+          <button type="submit">Send</button>
+        </Form>
+      )}
+    </Formik>
   );
 }
-
 export default ForgotPassword;
+
+    // <form onSubmit={forgotAction}>
+    //   Input email address, to send the confirmation code
+    //   <br />
+    //   <input type="email" name="email" />
+    //   <br />
+    //   <button type="submit">Submit</button>
+    // </form>
+
+
+
--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -1,39 +1,97 @@
-/* eslint-disable no-alert */
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Formik, Form, Field } from 'formik';
+import YupPassword from 'yup-password';
+import * as Yup from 'yup';
+import http from '../helpers/http'
+
+YupPassword(Yup);
 
 function ResetPassword() {
+
   const navigate = useNavigate();
-  const resetAction = async (e) => {
+
+  const basicAuthSchema = Yup.object().shape({
+    email: Yup.string().email('Email is not valid').required(),
+    code: Yup.number().min(6).required(),
+    newPassword: Yup.string().password().required(),
+  });
+
+  const submitAction = async (values) => {
     try {
-      e.preventDefault();
+      const form = new URLSearchParams(values);
+      await http().post('/auth/reset-password', form.toString());
       navigate('/login');
     } catch (err) {
       window.alert(err.response.data.message);
     }
   };
-
   return (
-    <form onSubmit={resetAction}>
-      Code :
-      <br />
-      <input type="number" name="code" />
-      <br />
-      Email :
-      <br />
-      <input type="email" name="email" />
-      <br />
-      New Password :
-      <br />
-      <input type="password" name="newPassword" />
-      <br />
-      Confirm Password :
-      <br />
-      <input type="password" name="confirmPassword" />
-      <br />
-      <button type="submit">Submit</button>
-    </form>
+    <Formik
+      initialValues={{
+        email: '',
+        code: '',
+        newPassword: '',
+        confirmPassword: '',
+      }}
+      validationSchema={basicAuthSchema}
+      onSubmit={submitAction}
+    >
+      {({ errors, touched }) => (
+        <Form>
+          Secret Code :
+          <Field type="text" name="code" />
+          <br />
+          {errors.code && touched.code ? (
+            <div>{errors.code}</div>
+          ) : null}
+          <br />
+          Email :
+          <Field type="text" name="email" />
+          <br />
+          {errors.email && touched.email ? (
+            <div>{errors.email}</div>
+          ) : null}
+          <br />
+          New Password :
+          <Field type="password" name="newPassword" />
+          <br />
+          {errors.newPassword && touched.newPassword ? (
+            <div>{errors.newPassword}</div>
+          ) : null}
+          <br />
+          Confirm Password :
+          <Field type="password" name="confirmPassword" />
+          <br />
+          {errors.confirmPassword && touched.confirmPassword ? (
+            <div>{errors.confirmPassword}</div>
+          ) : null}
+          <br />
+          <button type="submit">Send</button>
+        </Form>
+      )}
+    </Formik>
   );
 }
 
 export default ResetPassword;
+    // <form onSubmit={resetAction}>
+    //   Code :
+    //   <br />
+    //   <input type="number" name="code" />
+    //   <br />
+    //   Email :
+    //   <br />
+    //   <input type="email" name="email" />
+    //   <br />
+    //   New Password :
+    //   <br />
+    //   <input type="password" name="newPassword" />
+    //   <br />
+    //   Confirm Password :
+    //   <br />
+    //   <input type="password" name="confirmPassword" />
+    //   <br />
+    //   <button type="submit">Submit</button>
+    // </form>
+
