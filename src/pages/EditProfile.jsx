d516ef026711b76c8619b12d0aec79983ee1d3c3
rizalarif <rizal.arif2001@gmail.com>
2022-11-06 22:00:20 +0700

    Style: Profile and Edit Profile

--- src/pages/EditProfile.jsx
+++ src/pages/EditProfile.jsx
@@ -65,6 +65,38 @@ function EditProfile() {
 
   return (
     <>
+    {/* --- Navbar Section Start --- */}
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost btn-circle">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+          </label>
+          <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/profile">Profile</Link></li>
+          </ul>
+        </div>
+      </div>
+      <div className="navbar-center">
+        <a href='https://reactjs.org/' className="btn btn-ghost normal-case text-xl">EDIT PROFILE</a>
+      </div>
+      <div className="navbar-end">
+        <button className="btn btn-ghost btn-circle">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+        </button>
+        <button className="btn btn-ghost btn-circle">
+          <div className="indicator">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /></svg>
+            <span className="badge badge-xs badge-primary indicator-item"></span>
+          </div>
+        </button>
+      </div>
+    </div>
+    {/* --- Navbar Section End --- */}
+
+    {/* --- Hero Section Start --- */}
+    <div className='h-auto flex justify-center items-center'>
       {userProfile?.picture && <img style={{ width : '250px', height : '100%' }} src={`http://localhost:8888/assets/uploads/${userProfile?.picture}`} alt={userProfile?.fullName} />}
       <Formik
         initialValues={{
@@ -76,33 +108,52 @@ function EditProfile() {
         onSubmit={submitAction}
       >
         {({ errors, touched }) => (
-          <Form>
-            Full Name :
-            <Field type="text" name="fullName" />
+          <Form className='mt-10'>
+            <label htmlFor="fullName">Full Name</label> <br />
+            <Field type="text" name="fullName" className="input input-bordered w-full max-w-xs hover:input-primary" />
             <br />
             {errors.fullName && touched.fullName ? (
               <div>{errors.fullName}</div>
             ) : null}
             <br />
-            Birth Date :
-            <Field type="text" name="birthDate" />
+
+            <label htmlFor="birthDate">Birth Date</label>
+            <Field type="text" name="birthDate" className="input input-bordered w-full max-w-xs hover:input-primary" />
             <br />
             {errors.birthDate && touched.birthDate ? (
               <div>{errors.birthDate}</div>
             ) : null}
             <br />
-            Picture :
-            <Field type="file" name="picture" />
+
+            <label htmlFor="picture">Picture Profile</label><br />
+            <Field type="file" name="picture" className="file-input file-input-bordered file-input-warning w-full max-w-xs"/>
             <br />
-            {errors.picture && touched.picture ? (
+            {errors.picture && touched.picture? (
               <div>{errors.picture}</div>
             ) : null}
             <br />
-            <button type="submit">Save</button>
+
+            <button type="submit" className='btn mt-5 mb-12 btn-primary block w-full'>Save</button>
           </Form>
         )}
       </Formik>
-      <Link to="/profile">Go to profile page</Link>
+    </div>
+    {/* --- Hero Section End --- */}
+
+    {/* // --- Footer Start --- */}
+    <footer className="footer items-center p-4 bg-neutral text-neutral-content">
+      <div className="items-center grid-flow-col">
+        <svg width="36" height="36" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd" className="fill-current"><path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path></svg>
+        <p>Copyright © 2022 - All right reserved</p>
+      </div>
+      <div className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+        <a href='https://twitter.com/rizalarf_/' target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path></svg>
+        </a>
+        <a href='https://www.youtube.com/channel/UCJx648lb03ADZZwV8MmjKcQ' target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></a>
+        <a href='https://instagram.com/rizalarf_/' target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
+      </div>
+    </footer>
+    {/* // --- Footer End --- */}
     </>
   );
 }
